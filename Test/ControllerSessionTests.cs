e57a21bd2847fe2d287b8b88467c3af2a069f081
Oskar Henriksson <oskar.henriksson@se.abb.com>
2019-11-22 16:51:03 +0100

    Add ControllerSessionTests using NUnit

---
+++ Test/ControllerSessionTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using RWS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstrollerSessionTests
+{
+    [TestFixture]
+    class ControllerSessionTests
+    {
+        //Requires a running VC
+        [Test]
+        public void GetIOSignals_Localhost_HasValue()
+        {
+            //Arrange
+            ControllerSession rwsCs1 = new ControllerSession("localhost");
+
+            //Act
+            var ios = rwsCs1.RobotWareService.GetIOSignals().Embedded.State;
+
+            //Assert
+            Assert.IsNotEmpty(ios);
+            Assert.IsNotNull(ios.First().LValue);
+        }
+    }
+}
