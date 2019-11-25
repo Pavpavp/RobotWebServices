cda74bb0e48495878ada05bbb83b86d11c37bed2
Pav <pavel.epost@gmail.com>
2019-11-25 20:23:32 +0100

    .

--- ConsoleAppTest/Program.cs
+++ ConsoleAppTest/Program.cs
@@ -27,23 +27,23 @@ namespace ConsoleAppTest
 
             ControllerSession rwsCs1 = new ControllerSession(new Address("localhost:80"));
 
-            await rwsCs1.UserService.RequestRmmpAsync(Enums.Privilege.MODIFY).ConfigureAwait(false);
-            var rmmpState = await rwsCs1.UserService.GetRmmpStateAsync().ConfigureAwait(false);
-            await rwsCs1.UserService.RegisterUserAsync("SEPARIA", "RobotStudio", "SWE", Enums.LoginType.LOCAL).ConfigureAwait(false);
-            await rwsCs1.UserService.GrantOrDenyRmmpAsync(rmmpState.Embedded.State.First().UserID, Enums.Privilege.MODIFY).ConfigureAwait(false);
+            //await rwsCs1.UserService.RequestRmmpAsync(Enums.Privilege.MODIFY).ConfigureAwait(false);
+            //var rmmpState = await rwsCs1.UserService.GetRmmpStateAsync().ConfigureAwait(false);
+            //await rwsCs1.UserService.RegisterUserAsync("SEPARIA", "RobotStudio", "SWE", Enums.LoginType.LOCAL).ConfigureAwait(false);
+            //await rwsCs1.UserService.GrantOrDenyRmmpAsync(rmmpState.Embedded.State.First().UserID, Enums.Privilege.MODIFY).ConfigureAwait(false);
             //rwsCs1.RobotWareService.MastershipRequest();
             //rwsCs1.UserService.CancelHeldOrRequestedRmmp();
             //rwsCs1.ControllerService.Restart(Enums.RestartMode.RESTART);
 
 
 
-            //var io0 = GetIOSignalsAsync(rwsCs1).Result.Embedded.State[0];
+            var io0 = rwsCs1.RobotWareService.GetIOSignalsAsync().Result;
 
-            //io0.OnValueChanged += IOSignal_ValueChanged;
+            io0.Embedded.State[0].OnValueChanged += IOSignal_ValueChanged;
             Console.ReadKey();
 
-            //io0.OnValueChanged -= IOSignal_ValueChanged;
-            //Console.ReadKey();
+            io0.Embedded.State[0].OnValueChanged -= IOSignal_ValueChanged;
+            Console.ReadKey();
 
 
 
@@ -51,18 +51,6 @@ namespace ConsoleAppTest
             //var sdf = rwsCs1.RobotWareService.GetMotionSystem();
             //rwsCs.FileService.UploadFile(@"C:/Users/SEPARIA/Downloads/Sync14050W.pgf", "$home/Sync14050W.pgf", true);  //Replace with your paths
 
-        }
-
-        private async static Task<BaseResponse<IOSignalsState>> GetIOSignalsAsync(ControllerSession rwsCs1)
-        {
-            return await rwsCs1.RobotWareService.GetIOSignalsAsync().ConfigureAwait(false);
-        }
-
-        private async static void RequestRmmpAsync(ControllerSession rwsCs1)
-        {
-
-
-            ;
         }
 
         private static void IOSignal_ValueChanged(object sender, IOEventArgs args)
